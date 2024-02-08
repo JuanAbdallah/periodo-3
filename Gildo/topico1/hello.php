b0d90a210f1654ea14412757af74b367f8d02b07
Juan <juanabdallahritti@gmail.com>
2024-02-08 20:48:22 -0300

    1

--- Gildo/topico1/hello.php
+++ Gildo/topico1/hello.php
@@ -1,5 +1,5 @@
 <?php
   echo("Faculdades Integradas <b>Vianna Junior</b><br>");
   echo("Disciplina de :\n");
-  echo("Linguagem de Programação Web");     
+  echo("Linguagem de Programação Web");    
 ?>
