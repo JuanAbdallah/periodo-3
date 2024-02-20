28eaa57e9a912b3138a625d0ea9a1259d2a642c3
Juan Abdallah <juanabdallahritti@gmail.com>
2024-02-19 21:25:20 -0300

    1

--- Gildo/IA/arvore/index.js
+++ Gildo/IA/arvore/index.js
@@ -1,4 +1,13 @@
 import Arvore from "./Arvore.js"
-import No from "No.js";
+import No from "./No.js";
 
 let arvore = new Arvore('A');
+
+
+let noB = new No("B");
+let noC = new No("C");
+
+arvore.raiz.adicionaFilho(noB);
+arvore.raiz.adicionaFilho(noC);
+
+console.log(arvore);
