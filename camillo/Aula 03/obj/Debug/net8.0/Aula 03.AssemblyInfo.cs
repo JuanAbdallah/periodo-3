8dafc8ebea6dc5923a6d2afa73bbf64b041ba329
Juan <juanabdallahritti@gmail.com>
2024-02-23 21:49:13 -0300

    atts

--- camillo/Aula 03/obj/Debug/net8.0/Aula 03.AssemblyInfo.cs
+++ camillo/Aula 03/obj/Debug/net8.0/Aula 03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula 03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb83a2efaf65927cf676ce76eaea835d7f8b7673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula 03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula 03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
