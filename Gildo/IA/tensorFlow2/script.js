9a809e9612dbd444a55ba57d12c8f55f3971e504
Juan Oliveira <juanabdallahritti@gmail.com>
2024-04-24 20:57:55 -0300

    atts

--- Gildo/IA/tensorFlow2/script.js
+++ Gildo/IA/tensorFlow2/script.js
@@ -30,6 +30,12 @@ async function run(){
             height: 300
         }
     )
+     //converte os dados em um formulário que podemos usar para treinamento
+     const tensorData = convertToTensor(data);
+     const{inputs,labels} = tensorData;
+     //treina modelo
+     await trainModel(model,inputs,labels);
+     console.log("treino completo")
 }
 document.addEventListener('DOMContentLoaded',run)
 
@@ -50,5 +56,76 @@ function createModel(){
 //cria modelo
 const model = createModel();
 
+
+//converter os dados de entrada em tensores que podemos usar para o ML.Tambem faremos os embaralhamento e a normalização 
+//dos dados MPG(Milhas por galão) no eixo y
+function convertToTensor(data){
+
+    return tf.tidy(()=>{
+        //embaraçha os dados
+        tf.util.shuffle(data)
+        //converta os dafos em tensor
+        const inputs = data.map((d) => d.horsepower)
+        const labels = data.map((d) => d.mpg )
+
+        const inputTensor = tf.tensor2d(inputs,[inputs.length,1]);
+        const labelTensor = tf.tensor2d(labels,[labels.length,1]);
+
+        //normalize os dados para o intervalo 0 - 1 usando escala min-max
+        const inputMax = inputTensor.max();
+        const inputMin = inputTensor.min();
+        const labelMax = labelTensor.max();
+        const labelMin = labelTensor.min();
+
+        const normalizedInputs = inputTensor
+        .sub(inputMin)
+        .div(inputMax.sub(inputMin));
+
+        const normalizedLabels = labelTensor
+        .sub(labelMin)
+        .div(labelMax.sub(labelMin));
+
+        return{
+            inputs: normalizedInputs,
+            labels: normalizedLabels,
+            //retorne os limites minimos e máximos para podermos usar
+            inputMax:inputMax,
+            inputMin: inputMin,
+            labelMax: labelMax,
+            labelMin: labelMin
+        }
+
+    })
+
+}
 //exibe modelo
 tfvis.show.modelSummary({name: "Modelo"},model);
+
+document.addEventListener("DOMContentLoaded", run);
+
+async function trainModel(model, inputs, labels){
+    //prepara o modelo para o treinamento
+
+    model.compile({
+        optimizer: tf.train.adam(),
+        loss: tf.losses.meanSquaredError, //ver o quão certo ou errado está
+        metrics: ["mse"],//ver a porcentagem de acertos
+    });
+    //começar treinamento
+    //cada vez que treinar vai passar 32 pontos
+    const batchSize = 32;
+    const epochs = 50 //50 etapas de treinamento ou seja treina 50 vezes
+    //começar treino
+    return await model.fit(inputs,labels,{
+        batchSize,
+        epochs,
+        shuffle: true,//acabou de treinar embaralha
+        callbacks:tfvis.show.fitCallbacks(
+            {name: "performance de treinamento"},
+            ["loss","mse"],
+            {height: 200, callbacks:["oneEpochEnd"]}
+            ),
+    });
+   
+
+}
